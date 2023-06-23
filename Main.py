d973bb480f6018021ee8d7eef626db2ca68fc0fb
anonymous <101496703+svsapple77@users.noreply.github.com>
2023-06-23 20:05:21 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+from string import digits,punctuation,ascii_letters
+from random import choices
+class password_generator:
+    def __init__(self):
+        self.data = digits+punctuation+ascii_letters
+    def generate(self):
+        length = int(input("Enter length of the password : "))
+        weight = [3]*(len(digits)) + [2]*(len(punctuation)) + [1]*(len(ascii_letters))
+        rpass = choices(self.data,weights=weight,k=length)
+        random_pass = ''.join(str(i) for i in rpass)
+        print("Suggested password =",random_pass)
+
+def main():
+    ob = password_generator()
+    ob.generate()
+main()
