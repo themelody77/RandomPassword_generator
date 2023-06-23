af72d21a805e284df103600af9c9ec870409cacc
anonymous <101496703+svsapple77@users.noreply.github.com>
2023-06-23 20:25:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ class password_generator:
     def __init__(self):
         self.data = digits+punctuation+ascii_letters
     def generate(self):
-        length = int(input("Enter length of the password : "))
+        length = int(input("Enter the length of the password : "))
         weight = [3]*(len(digits)) + [2]*(len(punctuation)) + [1]*(len(ascii_letters))
         rpass = choices(self.data,weights=weight,k=length)
         random_pass = ''.join(str(i) for i in rpass)
